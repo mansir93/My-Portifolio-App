1213f03ee52eec89a01a11e6a3cbb58676fcaaa1
Mansir <mansiraziz93@gmail.com>
2023-05-13 13:53:23 +0000

    cleaning codes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import './App.css';
-import { Aboutme } from './component/AboutMe/Aboutme';
-import Contactme from './component/ContactMe/Contactme';
-import Home from './component/Home/Home';
-import Navbar from './component/Navbar/Navbar';
-import Resume from './component/Resume/Resume';
-import Testimonial from './component/Testimonial/Testimonial';
 
 
 
@@ -13,17 +7,17 @@ function App() {
   return (
     <div className="App">
 
-      <Navbar />
+      {/* <Navbar /> */}
 
-      <Home />
+      {/* <Profile /> */}
 
-      <Aboutme />
+      {/* <Aboutme /> */}
 
       {/* <Resume /> */}
 
       {/* <Testimonial /> */}
 
-      {/* <Contactme /> */}
+      {/* <Contactoption /> */}
       
     </div>
   );
